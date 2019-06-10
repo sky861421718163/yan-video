9d1ed11964aeb3c56dddcc9458ac1a8a79fe8518
Your Name <you@example.com>
2019-06-11 00:49:26 +0800

    dfa

--- yan-video/pages/mine/mine.js
+++ yan-video/pages/mine/mine.js
@@ -1,5 +1,5 @@
 // pages/main/main.js
-const app = getApp();
+const app = getApp()
 Page({
 
   /**
@@ -14,42 +14,109 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (params) {
     var me = this;
     var user = app.userInfo;  //全局对象
   },
-  logout:function(){
-    var user = app.userInfo;
-    var serverUrl = app.serverUrl;
-    wx.request({
-      url: serverurl+'loginOut?userId='+user.id,
-      method:"post",
-      data:{
+  logout: function () {
+     var user = app.userInfo;
+    //var user = app.getGlobalUserInfo();
 
-      },
-      header:{
+    var serverUrl = app.serverUrl;
+    wx.showLoading({
+      title: '请等待...',
+    });
+    // 调用后端
+    wx.request({
+      url: serverUrl + '/logout?userId=' + user.id,
+      method: "POST",
+      header: {
         'content-type': 'application/json' // 默认值
       },
-      success:function(res){
+      success: function (res) {
         console.log(res.data);
         wx.hideLoading();
-        if(res.data.starus == 200){
-          app.userInfo = null;
+        if (res.data.status == 200) {
+          // 登录成功跳转 
           wx.showToast({
             title: '注销成功',
-            icon:"success",
-            duration:3000
+            icon: 'success',
+            duration: 2000
           });
+           app.userInfo = null;
+          // 注销以后，清空缓存
+          //wx.removeStorageSync("userInfo")
+          // 页面跳转
           wx.redirectTo({
             url: '../userLogin/login',
           })
-        }else if(res.data.status == 502){
-          wx.showToast({
-            title: '注销失败',
-            icon:"none",
-            duration:3000
-          })
         }
+      }
+    })
+  },
+
+  //上传头像
+  changeFace: function () {
+    var me = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album'],
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths);
+
+        wx.showLoading({
+          title: '上传中...',
+        })
+        var serverUrl = app.serverUrl;
+        // fixme 修改原有的全局对象为本地缓存
+        //var userInfo = app.getGlobalUserInfo();
+
+        wx.uploadFile({
+          url: serverUrl + '/user/uploadFace?userId=' +app.userInfo.id,  //app.userInfo.id,
+          filePath: tempFilePaths[0],
+          name: 'file',
+          header: {
+            'content-type': 'application/json', // 默认值
+          },
+          success: function (res) {
+            var data = JSON.parse(res.data);
+            console.log(data);
+            wx.hideLoading();
+            if (data.status == 200) {
+              wx.showToast({
+                title: '上传成功!~~',
+                icon: "success"
+              });
+
+              var imageUrl = data.data;
+              me.setData({
+                faceUrl: serverUrl + imageUrl
+              });
+
+            } else if (data.status == 500) {
+              wx.showToast({
+                title: data.msg
+              });
+            } else if (res.data.status == 502) {
+              wx.showToast({
+                title: res.data.msg,
+                duration: 2000,
+                icon: "none",
+                success: function () {
+                  wx.redirectTo({
+                    url: '../userLogin/login',
+                  })
+                }
+              });
+
+            }
+
+          }
+        })
+
+
       }
     })
   }
